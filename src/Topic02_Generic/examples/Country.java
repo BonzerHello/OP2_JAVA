937e4afc8a69a047459c71bcf5b62d0e6035e1d1
BonzerHello <tillin@besonet.ch>
2017-05-18 22:00:27 +0200

    new files

---
+++ src/Topic02_Generic/examples/Country.java
@@ -0,0 +1,20 @@
+package Topic02_Generic.examples;
+
+public interface Country {
+
+	public static class Germany implements Country {
+
+		@Override
+		public String getLanguage() {
+			return "";
+		}}
+	public static class France implements Country {
+
+		@Override
+		public String getLanguage() {
+			return "";
+		}}
+	
+	public String getLanguage();
+
+}
